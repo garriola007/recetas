e9626334cd3825a2092497c103a115c3d9786ba8
garriola007 <gustavo.arriola@gmail.com>
2024-09-06 17:36:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+from openai import OpenAI
+from fpdf import FPDF
+from PIL import Image
+import requests
+from io import BytesIO
 
 key = st.text_input('input your Key', type='password')
 
