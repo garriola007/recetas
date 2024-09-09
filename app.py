41d5a4825a42f23798d8d6c10ae58ff4bd37715c
garriola007 <gustavo.arriola@gmail.com>
2024-09-09 09:27:01 -0300

    generar receta

--- app.py
+++ app.py
@@ -8,3 +8,28 @@ from io import BytesIO
 key = st.text_input('input your Key', type='password')
 
 st.title(key)
+
+
+def generar_receta(ingredientes):
+    system_prompt = f'''
+        Eres un chef de primera clase premiado con estrellas Michellin.
+        '''
+    user_prompt = f'''
+        Crea una receta detallada unicamente con los siguientes ingredientes: { ', '.join(ingredientes) }
+        Por favor, formate la receta de la siguiente manera:
+        Título de la receta:
+        Ingredientes de la receta con tamaño y porción:
+        Lista de instrucciones para la receta:
+        '''
+    response = client.Chat.Completion.create(
+            model = 'gpt-4o',
+            messages=[
+                {'role': 'system', 'content': system_prompt},
+                {'role': 'user', 'content': user_prompt}
+            ],
+            max_tokents = 1020,
+            temperatura = 0.9     
+    )
+
+    return response.choices[0].message.content
+
