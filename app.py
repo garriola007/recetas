70dafc70a3e4a8a6580c4fe7bb0a2830afdb6988
garriola007 <gustavo.arriola@gmail.com>
2024-09-09 10:12:10 -0300

    codigo casi completo

--- app.py
+++ app.py
@@ -33,3 +33,51 @@ def generar_receta(ingredientes):
 
     return response.choices[0].message.content
 
+def obtener_nombre_receta(texto):
+    lineas = texto.splitlines()
+    return lineas[1]
+
+def generar_imagen(titulo_receta):
+    promt = f'''
+        Genera una imagen fotorealista del plato final titulado: "{ titulo_receta }".
+        El plato debe estar bellamente presentado en un plato de cerámica con un enfoque cercano en las texturas y colores.
+        de los ingredientes.
+        La ambientacion debe ser una mesa de madera con iluminacion natural para resaltar las características apetitosas de la comida. 
+        Asegurate de que la imagen capture los colores ricos y vibrantes y los detalles intrincados de la comida, haciendola parecer recién preparada y lista para comer. 
+        La imagen debe estar en formato JPG.
+    '''
+    response = client.images.generate(
+        model = 'Dalle-e-3',
+        promt = promt,
+        style = 'vivid',
+        size = '1024x1024',
+        quality = 'high',
+        n=1
+    )
+    return response.data[0].url
+
+def guardar_pdf(titulo_receta, receta, imagen_url):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font(family: "Arial", size=12)
+
+    pdf.set_font(family: 'Arial', style='B', size=15)
+    pdf.cell(w:0, h:10, txt=titulo_receta, border=0, align="C")
+
+    response = requests.get(imagen_url)
+    img = Image.Open(BytesIO(response.content)).convert('RGB')
+    img_path = f"{titulo_receta.replace(' ', '_')}".jpg'
+    img.save(img_path, format:'JPEG')
+
+    pdf.ln(10)
+    img_width = pdf.image(img_path, w=(pdf.w -img_width)/2, w=img_width, type='JPEG')
+    pdf.ln(10)
+
+    pdf.set_font(family:'Arial', size=12)
+    for line in receta.split('\n'):
+        pdf.multi_cell(w: 0, h:10, line)
+
+    pdf_file = "receta.pdf"
+    pdf.output(pdf_file)
+
+    return pdf_file
